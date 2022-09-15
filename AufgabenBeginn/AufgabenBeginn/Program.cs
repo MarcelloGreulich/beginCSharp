dcf9fe8ae0ba4bded282db41d25fd3ab3a8b9c39
Marcello, Greulich <marcello.greulich@tecalliance.net>
2022-09-15 11:29:37 +0200

    initialize two numbers and sum them #1

--- AufgabenBeginn/AufgabenBeginn/Program.cs
+++ AufgabenBeginn/AufgabenBeginn/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AufgabenBeginn
+namespace Uebungen
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //variable
+            int zahl1 = 3;
+            int zahl2 = 5;
+
+            //calculate
+            int zahl3 = zahl1+zahl2;
+
+            //print sum
+            Console.WriteLine("Die Summe ist: " + zahl3);
+
+            //end Programm
+            Console.ReadKey();  
+
         }
     }
 }
