39d8f8977c7e5be06bb7fde1774fdd2e2fa29bf3
Marcello, Greulich <marcello.greulich@tecalliance.net>
2022-09-15 11:34:11 +0200

    ask user for number and store them #2

--- AufgabenBeginn/AufgabenBeginn/Program.cs
+++ AufgabenBeginn/AufgabenBeginn/Program.cs
@@ -10,15 +10,17 @@ namespace Uebungen
     {
         static void Main(string[] args)
         {
-            //variable
-            int zahl1 = 3;
-            int zahl2 = 5;
+            //Ask user for number
+            Console.WriteLine("Geben sie die erste Zahl ein");
+            int zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Geben sie die zweite Zahl ein");
+            int zahl2 = Convert.ToInt32(Console.ReadLine());
 
             //calculate
             int zahl3 = zahl1+zahl2;
 
             //print sum
-            Console.WriteLine("Die Summe ist: " + zahl3);
+            Console.WriteLine("Die Summe ihrer Zahlen ist: " + zahl3);
 
             //end Programm
             Console.ReadKey();  
