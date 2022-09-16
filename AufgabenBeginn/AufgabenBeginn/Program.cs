8c9dc5b459ae7bf34f7d1b73ad2441a64e590e4a
Marcello, Greulich <marcello.greulich@tecalliance.net>
2022-09-16 08:33:52 +0200

    hangman #14

--- AufgabenBeginn/AufgabenBeginn/Program.cs
+++ AufgabenBeginn/AufgabenBeginn/Program.cs
@@ -11,10 +11,64 @@ namespace Uebungen
     {
         static void Main(string[] args)
         {
-            for (int iv = 0; iv < 5; iv++)
+
+            Console.WriteLine("gib mir ein wort");
+            string wort = Console.ReadLine();
+            Console.Clear();
+
+            var placeholder = new List<string>();
+
+            for (int i = 0; i < wort.Length; i++)
             {
-                Console.WriteLine(iv);
+                placeholder.Add(" _ ");
+                Console.Write(placeholder[i]);
             }
+
+            
+
+            int live = 10;
+            while ( live > 0)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Buchstaben wählen");
+                string buchstabe = Console.ReadLine();
+
+                char[] newWort = wort.ToCharArray();
+       
+                for (int x = 0; x < newWort.Length; x++)
+                {
+
+                    Console.WriteLine(newWort[x]);
+                    if (newWort[x]==Convert.ToChar(buchstabe))
+                    {
+                        Console.WriteLine("test");
+             
+                    }
+                    
+                }
+
+                if (wort.Contains(buchstabe))
+                {
+                    Console.WriteLine("Rchtig!");
+                    Console.WriteLine("Sie haben noch " + live + " übrig");
+                }
+                else { 
+                    Console.WriteLine("Falsch!");
+                    live--;
+                    Console.WriteLine("Sie haben noch " + live + " übrig");
+                }
+
+
+                
+            }
+
+
+            /*  _____
+              |   O
+              |  -|-
+             /\   /\
+            */
+
             Console.ReadKey();
         }
     }
